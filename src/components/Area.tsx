7638cc9f36e5c527196de567e7baafb84a20744d
o.podilko <opodilko122@gmail.com>
2025-01-28 14:13:06 +0200

    remove all files

--- src/components/Area.tsx
+++
@@ -1,8 +0,0 @@
-import {SafeAreaView} from 'react-native';
-import React from 'react';
-
-export default function Area({children}: {children: React.ReactNode}) {
-  return (
-    <SafeAreaView className="flex-1 bg-bg relative">{children}</SafeAreaView>
-  );
-}
